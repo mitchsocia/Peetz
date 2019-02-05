41f6e0b0786e9d72c416275c73d911a312acd7d3
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-02-05 14:42:06 -0500

    Add files, models, data to tableView viewController

--- ViewControllers/ToppingsListViewController.swift
+++ ViewControllers/ToppingsListViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
+// MARK: - Data
+
+var toppingsCategories = [
+    ToppingCategory(categoryName: "Vegetables", toppings: ["Tomato", "Onion", "Spinach", "Green Olive", "Black Olive", "Mild Pepper", "Green Pepper", "Mushroom", "Jalapeño", "Basil"]),
+    ToppingCategory(categoryName: "Meats", toppings: ["Pepperoni", "Ham", "Bacon", "Ground Beef", "Sausage", "Anchovie", "Chicken", "Salami"]),
+    ToppingCategory(categoryName: "Misc.", toppings: ["Pineapple", "Pickle", "Garlic"])
+]
+
 class ToppingsListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+
+    // MARK: - TableView Setup
+    
+    
+    
 
 
 }
