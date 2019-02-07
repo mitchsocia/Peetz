20a9e4a815d226e2146600ad5f6de4faf7b53b8f
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-02-07 13:46:44 -0500

    Adding functions to favorites table view

--- ViewControllers/FavoritesViewController.swift
+++ ViewControllers/FavoritesViewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 
 class FavoritesViewController: UITableViewController {
+    
+    var favoritesChoice = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoritesChoice.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favorite", for: indexPath)
+        cell.textLabel?.text = ""
         
+//        Once we have an array to hold the favorites
+//        let favoriteCell = favorites[indexPath.row]
+//        cell.textLabel?.text = favoriteCell
+//
+//        if favoritesChoice.contains(favoriteCell) {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+//
+        return cell
     }
 
     /*
