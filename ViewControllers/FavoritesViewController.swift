77fcf91af2263f38ba0c49acdd61ade0208c9029
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-02-07 13:38:51 -0500

    Added file and view controller for favorites list

---
+++ ViewControllers/FavoritesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FavoritesViewController.swift
+//  Peetz
+//
+//  Created by Mitchell Socia on 2/7/19.
+//  Copyright © 2019 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+
+    /*
+     Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+     Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+         Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+}
