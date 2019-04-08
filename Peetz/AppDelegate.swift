5e3da7f82664343e5d02ae49e36f05228675feb7
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-04-08 14:43:29 -0400

    Added api key method to the appdelegate method called didFinishLaunchingWithOptions

--- Peetz/AppDelegate.swift
+++ Peetz/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyD-WHD_W15X1TDVAf2mcJIzARb9CeF4KuY")
         return true
     }
 
