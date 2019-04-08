742a030656abfa2dbca068964516fbc4d1cbb8e4
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-04-08 15:03:50 -0400

    Added methods for getting current place. Using this as a test. Will refactor later

--- ViewControllers/LocationViewController.swift
+++ ViewControllers/LocationViewController.swift
@@ -19,8 +19,30 @@ class LocationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        placesClient = GMSPlacesClient.shared()
 
     }
     
-
+    @IBAction func getCurrentPlace(_ sender: UIButton) {
+        
+        placesClient.currentPlace { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place Error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = "____"
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n")
+                }
+            }
+        }
+        
+    }
+    
 }
