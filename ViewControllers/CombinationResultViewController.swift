ae7b2c912d9f306760797df96b668441b03873ab
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-04-22 10:57:29 -0400

    Combination screen displays two randomly chosen toppings from the array created by checkmark. 'Go back' button dismisses the view

--- ViewControllers/CombinationResultViewController.swift
+++ ViewControllers/CombinationResultViewController.swift
@@ -9,13 +9,12 @@
 import UIKit
 
 class CombinationResultViewController: UIViewController {
-
+    
     var toppingCombinations = [String]()
     
     @IBOutlet weak var toppingDisplayLabelOne: UILabel!
-  
-    @IBOutlet weak var toppingDisplayLabelTwo: UILabel!
     
+    @IBOutlet weak var toppingDisplayLabelTwo: UILabel!
     
     @IBAction func goBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -23,7 +22,6 @@ class CombinationResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         toppingDisplayLabelOne.text = removeDuplicates(array: toppingCombinations).randomElement()
         toppingDisplayLabelTwo.text = removeDuplicates(array: toppingCombinations).randomElement()
@@ -46,6 +44,6 @@ class CombinationResultViewController: UIViewController {
         }
         return result
     }
-  
-
+    
+    
 }
