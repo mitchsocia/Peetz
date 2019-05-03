504462734cf77671b8a755fad565f355e76c5a18
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-05-03 13:30:37 -0400

    Not working, not sure where to place the methods in my file

--- ViewControllers/LocationViewController.swift
+++ ViewControllers/LocationViewController.swift
@@ -23,34 +23,49 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager.delegate = self
-        if CLLocationManager.authorizationStatus() == .notDetermined {
-            locationManager.requestWhenInUseAuthorization()
-        }
-        placesClient = GMSPlacesClient.shared()
-        
-        
-        placesClient.currentPlace { (placeLikelihoodList, error) -> Void in
-            if let error = error {
-                print("Pick Place Error: \(error.localizedDescription)")
-                return
+        func createSpinnerView() {
+            let child = SpinnerViewController()
+            
+            addChild(child)
+            child.view.frame = view.frame
+            view.addSubview(child.view)
+            child.didMove(toParent: self)
+            
+            
+            
+            locationManager.delegate = self
+            if CLLocationManager.authorizationStatus() == .notDetermined {
+                locationManager.requestWhenInUseAuthorization()
             }
             
-            self.nameLabel.text = "No current place"
-            self.addressLabel.text = "____"
-            
-            // guard here
-            if let placeLikelihoodList = placeLikelihoodList {
-                let place = placeLikelihoodList.likelihoods.first?.place
-                if let place = place {
-                    
-                    self.nameLabel.text = place.name
-                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n")
+            placesClient = GMSPlacesClient.shared()
+            placesClient.currentPlace { (placeLikelihoodList, error) -> Void in
+                if let error = error {
+                    print("Pick Place Error: \(error.localizedDescription)")
+                    return
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    child.willMove(toParent: nil)
+                    child.view.removeFromSuperview()
+                    child.removeFromParent()
                     
                 }
+                self.nameLabel.text = "No current place"
+                self.addressLabel.text = "____"
+                
+                // guard here
+                if let placeLikelihoodList = placeLikelihoodList {
+                    let place = placeLikelihoodList.likelihoods.first?.place
+                    if let place = place {
+                        self.nameLabel.text = place.name
+                        self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n")
+                        
+                    }
+                }
             }
+            
         }
         
     }
-    
 }
