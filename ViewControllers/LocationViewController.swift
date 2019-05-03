ed6dcd43e2f505f5ef25c6a10b2ed7838862d37f
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-05-03 14:46:42 -0400

    Activity indicator now working

--- ViewControllers/LocationViewController.swift
+++ ViewControllers/LocationViewController.swift
@@ -17,55 +17,39 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
     
     var placesClient: GMSPlacesClient!
     
+    @IBOutlet weak var activity: UIActivityIndicatorView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        func createSpinnerView() {
-            let child = SpinnerViewController()
-            
-            addChild(child)
-            child.view.frame = view.frame
-            view.addSubview(child.view)
-            child.didMove(toParent: self)
-            
-            
-            
-            locationManager.delegate = self
-            if CLLocationManager.authorizationStatus() == .notDetermined {
-                locationManager.requestWhenInUseAuthorization()
-            }
-            
-            placesClient = GMSPlacesClient.shared()
-            placesClient.currentPlace { (placeLikelihoodList, error) -> Void in
-                if let error = error {
-                    print("Pick Place Error: \(error.localizedDescription)")
-                    return
-                }
-                
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    child.willMove(toParent: nil)
-                    child.view.removeFromSuperview()
-                    child.removeFromParent()
-                    
-                }
-                self.nameLabel.text = "No current place"
-                self.addressLabel.text = "____"
-                
-                // guard here
-                if let placeLikelihoodList = placeLikelihoodList {
-                    let place = placeLikelihoodList.likelihoods.first?.place
-                    if let place = place {
-                        self.nameLabel.text = place.name
-                        self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n")
-                        
-                    }
-                }
-            }
-            
+        activity.startAnimating()
+        locationManager.delegate = self
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
         }
         
+        placesClient = GMSPlacesClient.shared()
+        placesClient.currentPlace { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place Error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = "____"
+            
+            // guard here
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.activity.isHidden = true
+                    self.activity.stopAnimating()
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress
+                    
+                }
+            }
+        }
     }
 }
